e3da038adaa45f7daeaa841e02586993a6504924
AnashiKitsun <diamondguy9700@gmail.com>
2020-12-20 08:49:26 -0700

    Fixed something and added screenshot

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -2,20 +2,9 @@ function git_branch_name
   echo (git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
 end
 
-function git_is_dirty
-  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
-end
-
-function heartsteal_git_dirty
-  if [ git_is_dirty != /dev/null ]
-    echo ±
-  end
-end
-
 function heartsteal_git_branch
   set_color normal && set_color -o
   set branch (git_branch_name)
-  set branch $branch (heartsteal_git_dirty)
   if [ (git_branch_name) ]
     echo -s [ (set_color normal && set_color cyan) $branch (set_color normal && set_color -o) ]
   end
